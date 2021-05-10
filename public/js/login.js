45a86ec6d0474095841140589a03f56f2610dfcd
Hung Tang <hungtang@hotmail.com>
2021-05-09 21:01:32 -0700

    added more frontend js files

--- public/js/login.js
+++ public/js/login.js
@@ -52,7 +52,3 @@ async function signupFormHandler(event) {
 
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
-
-
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
